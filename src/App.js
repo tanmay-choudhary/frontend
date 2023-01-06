d62388738bf84c7287a0e869037f98264b855236
tanmay-choudhary <tanmaychoudhary4102@gmail.com>
2023-01-06 23:34:36 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
 import logo from './logo.svg';
 import './App.css';
-
+import {React,useState,useEffect} from 'react';
+import axios from 'axios';
 function App() {
+  // let componentMounted = true;
+  // const getData = () => {
+  //   const getProducts = async () => {
+  //     //setLoading(true);
+  //     const response = await fetch("http://localhost:5000/users");
+  //     if (componentMounted) {
+  //       const searchData = await response.clone().json();
+  //       console.log(searchData[1]);
+  //     }
+
+  //     return () => {
+  //       componentMounted = false;
+  //     };
+  //   };
+  //   getProducts();
+  // }
+  // getData();
+   const [posts, setPosts] = useState([]);
+
+   useEffect(() => {
+     axios
+       .get("http://localhost:5000/users")
+       .then((response) => {
+         console.log(response);
+         setPosts(response.data);
+       })
+       .catch((err) => {
+         console.log(err);
+       });
+   }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    </>
   );
 }
 
